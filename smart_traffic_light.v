3ded4a1bb2236936814e1269bed1e6ea477a18c7
ROLOLOL <127593514+rolance110@users.noreply.github.com>
2023-06-29 23:08:03 +0800

    modify

--- smart_traffic_light.v
+++ smart_traffic_light.v
@@ -1,5 +1,5 @@
 ` include "counter.v"
-` include "controler.v"
+` include "controller.v"
 module smart_traffic_light(RYG, LRYG, L, H, clk, reset,count_out);
 
 input clk, reset;
@@ -13,6 +13,6 @@ CreateSecond S(.clk(clk), .reset(reset), .Q(fout));
 
 counter C1(.clk(fout),.count(count_out), .reset(count_reset)); 
 	
-controler C0(.RYG(RYG), .LRYG(LRYG), .L(L), .H(H), .clk(clk), .reset(reset), .count_in(count_out), .count_reset(count_reset));
+controller C0(.RYG(RYG), .LRYG(LRYG), .L(L), .H(H), .clk(clk), .reset(reset), .count_in(count_out), .count_reset(count_reset));
 	
 endmodule
