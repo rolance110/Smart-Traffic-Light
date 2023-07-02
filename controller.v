c3021afc385145cecccf6307b36c87dc2e5bb73b
ROLOLOL <127593514+rolance110@users.noreply.github.com>
2023-07-02 23:42:55 +0800

    7/2

--- controller.v
+++ controller.v
@@ -1,30 +1,38 @@
 module controller (
 	input clk, 
-	input reset,
-	input [2:0] L,
-	input [2:0] H,
-
-	output reg [2:0] RYG, 
-	output reg [3:0] LRYG,
-	output reg  P,
-	
-	input count_in, 
-	output count_reset,
+	input rst,
+	input [2:0] main_num,
+	input [2:0] left_num,
+	input [2:0] sec_num,
+	input [2:0] p_num,
+	input m_emergency,
+	input s_emergency,
+	output reg [2:0] s_RYG, 
+	output reg [3:0] m_LRYG,
+	output reg  ped,
 	);
+
+reg [4:0] MaxTime ;
+reg [4:0] sec_count ;
+
+
+
+
+
 
 //finit state machine
 reg [2:0] ns,cs;// ns:nextstate  cs:currentstate
 parameter INIT =3'd0;//initial
-parameter M=3'd1;
+parameter M_G=3'd1;
 parameter M_L=3'd2;
-parameter S=3'd3;
-parameter PEDESTRIAN=3'd4;
+parameter S_G=3'd3;
+parameter P_G=3'd4;
 parameter M_Y=3'd5;
 parameter S_Y=3'd6;
 //set currentstate
-always@(posedge clk or posedge reset)
+always@(posedge clk or posedge rst)
 begin
-	if(reset)
+	if(rst)
 		cs <= INIT ;
 	else
 		cs <= ns ;
@@ -35,141 +43,112 @@ always@(*)
 begin
 	case(cs)
 		INIT:
-			ns=M;             
-		M:
-			if()
+			ns=M_G;             
+		M_G:
+			if(sec_count == MaxTime)
 				ns=M_Y;
 			else
 				ns=M;
 		M_Y:
-			if()
+			if( sec_count == MaxTime)
 				ns=M_L;
-			else if()
-				ns=S;//有可能沒亮左轉燈
+			else if( left_num == 3'd0)
+				ns=S_G;//有可能沒亮左轉燈
 			else
 				ns=M_Y;
 		M_L:
-			if()
+			if(sec_count == MaxTime)
 				ns=M_Y;
 			else
 				ns=M_L;
-		S:
-			if()
+		S_G:
+			if(sec_count == MaxTime)
 				ns=S_Y;
 			else
 				ns=S;
 		S_Y:
-			if()
-				ns=PEDESTRIAN;
-			else if()
-				ns=M;
+			if(sec_count == MaxTime)
+				ns=P_G;
+			else if(p_num == 3'd0)
+				ns=M_G;
 			else
 				ns=S_Y;
+		P_G:
+			if(sec_count == MaxTime)
+				ns=P_Y;
+			else
+				ns=P;
 		default:
 			ns=INIT ;
 
 	endcase
 end
-reg [4:0] MaxTime ;
 
-wire in_reset,ToEnd ;
-
-assign ToEnd = (MaxTime == count_in) ;//if run to max time
-
-assign in_reset = reset || (RYG[1] && LRYG[2]&& ToEnd);//
-
-assign count_reset = in_reset || ToEnd ;
-
-///////////output light(from current state)///////////////////
+//output light
 always @ (posedge clk or posedge rst)
 	case(cs)
-		3'd0 : begin 
-			LRYG <= 4'b0001 ;//Master road:G
-			RYG <= 3'b100 ;  //Branch road:R
+		M_G : begin 
+			m_LRYG <= 4'b0001 ;//Master road:G
+			s_RYG <= 3'b100 ;  //Secondary road:R
+			ped <= 1'b0 ;      //pedestrian:off
 		end 
-		3'd1 : begin 
-			LRYG <= 4'b0010 ;//Master road:Y
-			RYG <= 3'b100 ;  //Branch road:R
+		M_Y : begin 
+			m_LRYG <= 4'b0010 ;//Master road:Y
+			s_RYG <= 3'b100 ;  //Secondary road:R
+			ped <= 1'b0 ;      //pedestrian:off
 		end 	
-		3'd2 : begin 
-			LRYG <= 4'b1100 ;//Master road:L&R
-			RYG <= 3'b100 ;  //Branch road:R
+		M_L : begin 
+			m_LRYG <= 4'b1100 ;//Master road:L&R
+			s_RYG <= 3'b100 ;  //Secondary road:R
+			ped <= 1'b0 ;      //pedestrian:off
 		end
-		3'd3 : begin 
-			LRYG <= 4'b0010 ;//Master road:Y
-			RYG <= 3'b100 ;  //Branch road:R
+		S_G : begin 
+			m_LRYG <= 4'b0100 ;//Master road:R
+			s_RYG <= 3'b001 ;  //Secondary road:G
+			ped <= 1'b0 ;      //pedestrian:off
 		end
-		3'd4 : begin 
-			LRYG <= 4'b0100 ;//Master road:R
-			RYG <= 3'b001 ;  //Branch road:G
+		S_Y : begin 
+			m_LRYG <= 4'b0100 ;//Master road:R
+			s_RYG <= 3'b010 ;  //Secondary road:Y
+			ped <= 1'b0 ;      //pedestrian:off
 		end
-		3'd5 : begin 
-			LRYG <= 4'b0100 ;//Master road:R
-			RYG <= 3'b010 ;  //Branch road:Y
+		P_G : begin 
+			m_LRYG <= 4'b0001 ;//Master road:G
+			s_RYG <= 3'b100 ;  //Secondary road:R
+			ped <= 1'b1 ;      //pedestrian:on                      
 		end
-		3'd6 : begin 
-			LRYG <= 4'b0001 ;//Master road:G
-			RYG <= 3'b100 ;  //Branch road:R                      
-		end
-		3'd7 : begin 
-			LRYG <= 4'b0001 ;//Master road:G
-			RYG <= 3'b100 ;  //Branch road:R                        
+		default : begin 
+			m_LRYG <= 4'b1111 ;//Master road: error test
+			s_RYG <= 3'b100 ;  //Branch road:R
+			ped <= 1'b0 ;      //pedestrian:off
 		end
 	endcase
-
-always @ (cs)
+//second counter
+always@(posedge rst or posedge clk) begin
+	if (rst)begin
+		sec_count <= 5'd0 ;
+	end
+	else if (sec_count == MaxTime)begin
+		sec_count <= 5'd0 ;
+	end
+	else begin
+		sec_count <= sec_count + 5'd1 ;
+	end
+end
+//max time
+always @ (*)begin
 	case(cs) 
-		3'd0 : MaxTime <= 20;
-		3'd1 : MaxTime <= 3 ;
-		3'd2 : MaxTime <= 10;
-		3'd3 : MaxTime <= 3 ;
-		3'd4 : MaxTime <= 15;
-		3'd5 : MaxTime <= 3 ;
-		3'd6 : MaxTime <= 20;
-		3'd7 : MaxTime <= 20;
+		M_G : MaxTime = 5'd30 ;
+		M_Y : MaxTime = 5'd3 ;
+		M_L : MaxTime = 5'd12 ;
+		S_G : MaxTime = 5'd18 ;
+		S_Y : MaxTime = 5'd3 ;
+		P_G : MaxTime = 5'd18 ;
+		default : MaxTime = 5'd0 ;
 	endcase
-//////////////////////////////////////////////////////////////
+end
 
 
-////////set intitial state (from how many car on road)////////////
-always @ (H, L) 
-	case ({((L[0] && !H[0]) || (L[1] && !H[1]) || H[1]), H[0]})
-		3'd0 : cs0 <= (L[2] || H[2]) ? 3'd7 : 3'd0 ;
-		3'd1 : cs0 <= 3'd1 ;
-		3'd2 : cs0 <= 3'd6 ;
-		default : cs0 <= 3'd0 ;
-	endcase
-//////////////////////////////////////////////////////////////
 
-///////////set next state(decided by current state)///////////////
-always @ (posedge count_reset or posedge in_reset)
-   begin 
-	if (in_reset)
-		ns <= 0 ;
-	
-	else begin 
-		case(cs)
-			3'd0 : ns <= cs0 ;
-			3'd1 : ns <= (|H) ? 3'd2 : 3'd4 ;
-			3'd2 : ns <= 3'd3 ;
-			3'd3 : ns <= 3'd4 ;
-			3'd4 : ns <= 3'd5 ;
-			3'd5 : ns <= 3'd0 ;
-			3'd6 : ns <= 4'd1 ;
-			3'd7 : ns <= (H[1] || L[1]) ? 3'd1 : 3'd6 ;
-		endcase
-    end
-  end 
-//////////////////////////////////////////////////////////////
 
-//////////////////set current state///////////////////////////
-always @ (posedge clk or posedge in_reset) 
-  begin 
-	if (in_reset)
-		cs <= 0 ;
-		
-	else
-		cs <= ns ;
-  end 
-//////////////////////////////////////////////////////////////
 endmodule
