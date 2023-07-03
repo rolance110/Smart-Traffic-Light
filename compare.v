2f489be24ea11ff5820f0d88739ab57b4fed6680
ROLOLOL <127593514+rolance110@users.noreply.github.com>
2023-07-03 22:45:25 +0800

    compare finish

--- compare.v
+++ compare.v
@@ -3,17 +3,19 @@ module compare (
 	input [2:0] left_num,
 	input [2:0] sec_num,
 	input [2:0] p_num, 
-	output [2:0] absolute_num,
-	output m_more,
-	output l_zero,
-	output s_more,
-	output p_more,
+	output wire [2:0] absolute_num,
+	output wire m_more,
+	output wire l_zero,
+	output wire s_more,
+	output wire p_more,
 );
 
-	assign m_more = (main_num > left_num);
-	assign l_zero = (left_num == 0);
-	assign s_more = (sec_num > main_num);
-	assign p_more = (p_num > sec_num);
-	assign absolute_num = (m_more) ? (main_num - left_num) : (left_num - main_num);
+assign m_more = (main_num > (left_num<<1))? 1'b1 : 1'b0;
+assign l_zero = (left_num == 0)? 1'b1 : 1'b0;
+assign s_more = (sec_num == main_num)? 1'b1: 1'b0;
+assign p_more = (p_num>(main_num+sec_num))? 1'b1: 1'b0;
+assign absolute_num[2] = (main_num[2]==1)? 1'b1: 1'b0;
+assign absolute_num[1] = (sec_num==main_num)? 1'b1: 1'b0;
+assign absolute_num[0] = (left_num == 0)? 1'b1 : 1'b0;
 	
 endmodule
